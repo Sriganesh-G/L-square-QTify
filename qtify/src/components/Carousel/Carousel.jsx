13cdba790bd4920a0b695d46e940865b7eed4393
Sriganesh-G <sriganesh9647@gmail.com>
2024-08-27 21:48:55 +0530

    test case passing code

--- qtify/src/components/Carousel/Carousel.jsx
+++ qtify/src/components/Carousel/Carousel.jsx
@@ -1,55 +1,3 @@
-/* import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/navigation";
-import { Navigation } from "swiper/modules";
-import LeftArrow from "../../assets/leftButton.png";
-import RightArrow from "../../assets/rightButton.png";
-import styles from "./Carousel.module.css";
-import SongsCard from "../SongsCard/SongsCard";
-
-const Carousel = ({ newAlbums = [], songs = [] }) => {
-  const items = newAlbums.length > 0 ? newAlbums : songs;
-  const isSongs = songs.length > 0;
-
-  return (
-    <div className={styles.carouselContainer}>
-      <Swiper
-        className={styles.swiper}
-        slidesPerView={7}
-        spaceBetween={0}
-        modules={[Navigation]}
-        navigation={{
-          nextEl: `.${styles.nextArrow}`,
-          prevEl: `.${styles.prevArrow}`,
-        }}
-      >
-        {items.map((item) => (
-          <SwiperSlide key={item.id}>
-            <SongsCard
-              title={item.title}
-              image={item.image}
-              follows={item.follows}
-              likes={item.likes}
-              isSongs={isSongs}
-            />
-          </SwiperSlide>
-        ))}
-        <div className={styles.prevArrow}>
-          <img src={RightArrow} alt="Previous" />
-        </div>
-        <div className={styles.nextArrow}>
-          <img src={LeftArrow} alt="Next" />
-        </div>
-      </Swiper>
-    </div>
-  );
-};
-
-export default Carousel;
- */
-
-//-----------------------------------------------------
 import React, { useState, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
@@ -100,17 +48,15 @@ const Carousel = ({ newAlbums = [], songs = [] }) => {
             />
           </SwiperSlide>
         ))}
-        <div
-          className={`${styles.prevArrow} ${
-            isBeginning ? styles.disabled : ""
-          }`}
-        >
-          <img src={RightArrow} alt="Previous" />
-        </div>
-        <div className={`${styles.nextArrow} ${isEnd ? styles.disabled : ""}`}>
-          <img src={LeftArrow} alt="Next" />
-        </div>
       </Swiper>
+      <div
+        className={`${styles.prevArrow} ${isBeginning ? styles.disabled : ""}`}
+      >
+        <img src={RightArrow} alt="Previous" />
+      </div>
+      <div className={`${styles.nextArrow} ${isEnd ? styles.disabled : ""}`}>
+        <img src={LeftArrow} alt="Next" />
+      </div>
     </div>
   );
 };
